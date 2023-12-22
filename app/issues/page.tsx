79cc6fb42d95bd60baa42b373d215087bef54b10
Yougotcrushed <gettycrushed@gmail.com>
2023-12-22 10:00:33 +0100

    Fix filtering bugs

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -39,7 +39,10 @@ const issuePage = async ({ searchParams }: Props) => {
         <Table.Header>
           <Table.Row>
             {columns.map((column) => (
-              <Table.ColumnHeaderCell key={column.value}>
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
+              >
                 <NextLink
                   href={{
                     query: { ...searchParams, orderBy: column.value },
